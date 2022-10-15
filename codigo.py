b610780c66a694eb54181def289194ffcdadd904
pepe <jdavila@espol.edu.ec>
2022-10-14 21:24:18 -0500

    actualizado el documento

---
+++ codigo.py
@@ -0,0 +1,4 @@
+import streamlit as st
+st.title('Bienvenidos Ingenieros Petroleros de Machala')
+st.write('---')
+st.header('Esta página está destinada para los ingenieros de petroleos de origen Machaleño')
